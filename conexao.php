f7d6782e2a222bc6aa48064960a20d4ab0e9f1c3
pedroburguez <pedro.burguez@hotmail.com>
2017-03-05 17:12:06 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,5 +1,5 @@
 <?php 
 
-$conexao = mysqli_connect("localhost", "root", "", "teste");
+#$conexao = mysqli_connect("localhost", "root", "", "teste");
 
-#$conexao = mysqli_connect("mysql.hostinger.com.br", "u912684112_teste", "testeteste", "u912684112_teste");
+$conexao = mysqli_connect("mysql.hostinger.com.br", "u912684112_teste", "testeteste", "u912684112_teste");
